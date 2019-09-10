4f75e4784ff763ae33e762c9f178371f91a16a75
Victor Wang <victor_wang@sottop.cn>
2019-09-10 16:19:42 +0800

    detail

--- src/app/employee-card/employee-card.page.ts
+++ src/app/employee-card/employee-card.page.ts
@@ -11,15 +11,15 @@ export class EmployeeCardPage implements OnInit {
   constructor(private router: Router) { }
 
   employees: Array<any> = [
-    {name: '闫磊', avatar: 'yanlei.png', birthDay: '1992年5月7日', post: '劳务负责人', idNo: '640321199205071530', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '邵新平', avatar: 'shaoxinping.png', birthDay: '1982年11月3日', post: '劳务负责人', idNo: '640321198211030714', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '冯宏祥', avatar: 'fenghongxiang.png', birthDay: '1968年3月10日', post: '劳务负责人', idNo: '642123196803100514', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '孙兵', avatar: 'sunbing.png', birthDay: '1991年7月10日', post: '劳务负责人', idNo: '640321199107101732', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '殷艳君', avatar: 'yinyanjun.png', birthDay: '1992年8月14日', post: '劳务负责人', idNo: '640321199208141717', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '陈富保', avatar: 'chenfubao.png', birthDay: '1992年1月29日', post: '劳务负责人', idNo: '640321199201290314', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '邵银华', avatar: 'shaoyinhua.png', birthDay: '1971年6月5日', post: '劳务负责人', idNo: '642123197206050712', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '解建良', avatar: 'xiejianliang.png', birthDay: '1990年4月19日', post: '劳务负责人', idNo: '642224199004191216', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
-    {name: '吴建国', avatar: 'wujianguo.png', birthDay: '1978年11月23日', post: '劳务负责人', idNo: '642123197811232112', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '闫磊', avatar: 'yanlei.png', certification: '生产配电工程施工资格', birthDay: '1992年5月7日', post: '劳务负责人', idNo: '640321199205071530', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '邵新平', avatar: 'shaoxinping.png', certification: '生产配电工程施工资格', birthDay: '1982年11月3日', post: '劳务负责人', idNo: '640321198211030714', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '冯宏祥', avatar: 'fenghongxiang.png', certification: '生产配电工程施工资格', birthDay: '1968年3月10日', post: '劳务负责人', idNo: '642123196803100514', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '孙兵', avatar: 'sunbing.png', certification: '生产配电工程施工资格', birthDay: '1991年7月10日', post: '劳务负责人', idNo: '640321199107101732', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '殷艳君', avatar: 'yinyanjun.png', certification: '生产配电工程施工资格', birthDay: '1992年8月14日', post: '劳务负责人', idNo: '640321199208141717', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '陈富保', avatar: 'chenfubao.png', certification: '生产配电工程施工资格', birthDay: '1992年1月29日', post: '劳务负责人', idNo: '640321199201290314', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '邵银华', avatar: 'shaoyinhua.png', certification: '生产配电工程施工资格', birthDay: '1971年6月5日', post: '劳务负责人', idNo: '642123197206050712', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '解建良', avatar: 'xiejianliang.png', certification: '生产配电工程施工资格', birthDay: '1990年4月19日', post: '劳务负责人', idNo: '642224199004191216', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
+    {name: '吴建国', avatar: 'wujianguo.png', certification: '生产配电工程施工资格', birthDay: '1978年11月23日', post: '劳务负责人', idNo: '642123197811232112', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日', issuePost: ''},
   ];
 
   ngOnInit() {
