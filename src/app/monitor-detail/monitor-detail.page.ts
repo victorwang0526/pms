3f46b7828a94d57358579c87b353184984113fe1
Victor Wang <victor_wang@sottop.cn>
2019-09-10 17:16:32 +0800

    LIVE

--- src/app/monitor-detail/monitor-detail.page.ts
+++ src/app/monitor-detail/monitor-detail.page.ts
@@ -14,7 +14,18 @@ export class MonitorDetailPage implements OnInit {
   player: any;
 
   ngOnInit() {
-    this.player = new EZUIPlayer('myPlayer');
+//开始初始化监控地址
+    this.player = new EZUIPlayer({
+      id: 'myPlayer',
+      url: 'https://hls01open.ys7.com/openlive/dc76cdfbc3ad4d70af9b6714986451a2.m3u8',
+      autoplay: true,
+      accessToken: "at.afoq68vkd0lopygdbnmbvuem43ej80qp-8s190de6vs-0zyn3sy-ku3rabe6k",
+      width: 600,
+      height: 400,
+    });
+//播放
+    this.player.play();
+
   }
 
 }
