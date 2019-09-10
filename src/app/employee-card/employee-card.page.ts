f8f50787b2dee9b93132b37e44ccb2e5516b5959
Victor Wang <victor_wang@sottop.cn>
2019-09-10 14:45:33 +0800

    employee card and monitor

--- src/app/employee-card/employee-card.page.ts
+++ src/app/employee-card/employee-card.page.ts
@@ -9,6 +9,18 @@ export class EmployeeCardPage implements OnInit {
 
   constructor() { }
 
+  employees: Array<any> = [
+    {name: '闫磊', avatar: 'yanlei.png', birthDay: '1992年5月7日', post: '劳务负责人', idNo: '640321199205071530', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '邵新平', avatar: 'shaoxinping.png', birthDay: '1982年11月3日', post: '劳务负责人', idNo: '640321198211030714', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '冯宏祥', avatar: 'fenghongxiang.png', birthDay: '1968年3月10日', post: '劳务负责人', idNo: '642123196803100514', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '孙兵', avatar: 'sunbing.png', birthDay: '1991年7月10日', post: '劳务负责人', idNo: '640321199107101732', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '殷艳君', avatar: 'yinyanjun.png', birthDay: '1992年8月14日', post: '劳务负责人', idNo: '640321199208141717', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '陈富保', avatar: 'chenfubao.png', birthDay: '1992年1月29日', post: '劳务负责人', idNo: '640321199201290314', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '邵银华', avatar: 'shaoyinhua.png', birthDay: '1971年6月5日', post: '劳务负责人', idNo: '642123197206050712', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '解建良', avatar: 'xiejianliang.png', birthDay: '1990年4月19日', post: '劳务负责人', idNo: '642224199004191216', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+    {name: '吴建国', avatar: 'wujianguo.png', birthDay: '1978年11月23日', post: '劳务负责人', idNo: '642123197811232112', registeDate: '2019年2月', expired: '一年', issueDate: '2019年2月15日'},
+  ];
+
   ngOnInit() {
   }
 
--- src/app/monitoring/monitoring.page.ts
+++ src/app/monitoring/monitoring.page.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonitoringPage implements OnInit {
 
+  monitors = [
+    {channel: '1', name: '视频1@DS-7808NB-K2(C30163833)', status: '在线', liveStatus: '已开通', url: ''},
+    {channel: '2', name: '视频2@DS-7808NB-K2(C30163833)', status: '在线', liveStatus: '已开通', url: ''},
+    {channel: '3', name: '视频3@DS-7808NB-K2(C30163833)', status: '在线', liveStatus: '已开通', url: ''},
+    {channel: '4', name: '视频4@DS-7808NB-K2(C30163833)', status: '在线', liveStatus: '已开通', url: ''},
+    {channel: '5', name: '视频5@DS-7808NB-K2(C30163833)', status: '在线', liveStatus: '已开通', url: ''},
+    {channel: '6', name: '视频6@DS-7808NB-K2(C30163833)', status: '在线', liveStatus: '已开通', url: ''},
+    {channel: '7', name: '视频7@DS-7808NB-K2(C30163833)', status: '在线', liveStatus: '已开通', url: ''},
+    {channel: '8', name: '视频8@DS-7808NB-K2(C30163833)', status: '不在线', liveStatus: '已开通', url: ''},
+    {channel: '9', name: '视频9@DS-7808NB-K2(C30163833)', status: '不在线', liveStatus: '未开通', url: ''},
+    {channel: '10', name: '视频10@DS-7808NB-K2(C30163833)', status: '不在线', liveStatus: '未开通', url: ''},
+  ];
   constructor() { }
 
   ngOnInit() {
