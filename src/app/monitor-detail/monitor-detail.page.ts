9bb0a7190f0dac1959ba8e7220cd28ee85f4b2e3
Victor Wang <victor_wang@sottop.cn>
2019-09-10 16:49:00 +0800

    LIVE

--- src/app/monitor-detail/monitor-detail.page.ts
+++ src/app/monitor-detail/monitor-detail.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var EZUIPlayer: any;
+
 @Component({
   selector: 'app-monitor-detail',
   templateUrl: './monitor-detail.page.html',
@@ -9,7 +11,10 @@ export class MonitorDetailPage implements OnInit {
 
   constructor() { }
 
+  player: any;
+
   ngOnInit() {
+    this.player = new EZUIPlayer('myPlayer');
   }
 
 }
